4cb55f258dcc7d74c1ee2ac7c4179599b73592ec
3969okki <137705196+3969okki@users.noreply.github.com>
2025-01-06 23:08:34 +0900

    Update taiwaBot.py

--- taiwaBot.py
+++ taiwaBot.py
@@ -1,39 +1,45 @@
 import pathlib
 import textwrap
 import google.generativeai as genai
-import voice
+import make_voice
+import re
 from IPython.display import display
 from IPython.display import Markdown
 
 
 
 def to_markdown(text):
-  text = text.replace('•', '  *')
+  text = text.replace('(', '  )')
   return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
 
 API_KEY = 'AIzaSyD83xNcxVRtuJWePbPSrjCHswSasyzd7CY' 
 genai.configure(api_key=API_KEY)
 model = genai.GenerativeModel("gemini-1.5-flash")
 
-pre_text = "彼女みたいに返答して"
-
+speaker = 1
+giri_model = "甘えん坊な彼女みたいに返答して"
+speed = 2.0
 chat = model.start_chat(history=[])
-chat.send_message(pre_text)
+chat.send_message(giri_model)
 
 
 def taiwa_change(input_text):
 	#input_text = input("入力 >> ")
 	response = chat.send_message(input_text)
-	voice.synthesize_voice(response.text, speaker=1, filename="voicevox_output.wav")
+	make_voice.synthesize_voice(response.text, speaker = speaker,speed = speed, filename="voicevox_output.wav")
 	print(response.text)
 	return response.text
 
-"""
-def main():
-	while True:
-		input_text = input("入力 >> ")
-		taiwa_change(input_text)
+def text_taiwa(input_text):
+	tmpresponse = chat.send_message(input_text.content)
+	return re.sub(r'\(.*?\)', '', str(tmpresponse.text))
 
-if __name__ == "__main__":
-	main()
-"""
+def question(Q_text):
+	response = model.generate_content(Q_text)
+	chunks = split_string(response.text)
+	return chunks
+
+
+def split_string(input_string, max_length=2000):
+# 文字列をmax_lengthごとに分割   
+	return [input_string[i:i+max_length] for i in range(0, len(input_string), max_length)]

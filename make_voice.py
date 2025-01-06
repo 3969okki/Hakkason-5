4dfef0161efe1e641c9da00fdfc41a725c4b3c0c
3969okki <137705196+3969okki@users.noreply.github.com>
2025-01-06 23:07:16 +0900

    Update and rename voice.py to make_voice.py

--- make_voice.py
+++ make_voice.py
@@ -1,11 +1,15 @@
 import requests
+import re
 import json
 
+
+
 #VOICEVOXは音声作成後wavになって音声が作成される
-def synthesize_voice(text, speaker = 2, filename = "output.wav"):
+def synthesize_voice(word, speaker = 1,speed = 1.2, filename = "output.wav"):
 
     #テキストからの音声合成クエリ作成
     #クエリ データベースに送る命令文
+    text = re.sub(r'\(.*?\)', '', str(word))
     query_payload = {'text': text, 'speaker': speaker}
     query_response = requests.post(f'http://localhost:50021/audio_query', params=query_payload)
 
@@ -13,8 +17,10 @@ def synthesize_voice(text, speaker = 2, filename = "output.wav"):
         print(f"Error in audio_query: {query_response.text}")
         return
     
-    query = query_response.json()
 
+    query = query_response.json()
+    query['speed'] = speed  # speedを変更
+    
     #音声データの作成
     synthesis_payload = {'speaker' : speaker}
     synthesis_response = requests.post(f'http://localhost:50021/synthesis', params = synthesis_payload, json = query)
@@ -36,5 +42,7 @@ def main():
 if __name__ == '__main__':
     main()
 """
+
+
 
 
